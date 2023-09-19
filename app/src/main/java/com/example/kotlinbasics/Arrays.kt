31efc3787e5d97d9db2b86c6d4a6e586d75c95d6
Gursimarsingh12 <141723338+Gursimarsingh12@users.noreply.github.com>
2023-09-19 22:32:23 +0530

    Fundamentals of Kotlin

--- app/src/main/java/com/example/kotlinbasics/Arrays.kt
+++ app/src/main/java/com/example/kotlinbasics/Arrays.kt
@@ -1,10 +1,12 @@
 package com.example.kotlinbasics
 
 class Fruits(var FruitName: String, var price: Double)
+
 fun main(){
     // var array1: IntArray = intArrayOf(1,2,3,4,5)
     // var array1 = intArrayOf(1,2,3,4,5)
     var array1 = arrayOf(1,2,3,4,5)
+    println(array1)   //It gives the address of the first element of array1
 //    println("\n${array1.contentToString()}")
     print("Initial values: ")
     for(element in array1){
@@ -20,5 +22,17 @@ fun main(){
     for(element in array1){
         print("$element,")
     }
-
+    array1[1] = 12
+//    array1[7] = 13    -----> gives error of ArrayIndexOutOfBoundsException
+    println("")
+    var mutableArray = arrayOf(1, "John", 32, 53.0, "Doe", Fruits("Apple", 30.0))
+    println(mutableArray.contentToString())
+    var fruits = arrayOf(Fruits("Apple", 30.0), Fruits("Mango", 70.0))
+    for(fruit in fruits){
+        print("${fruit.FruitName} ")
+    }
+    println("")
+    for(index in fruits.indices){                       // .indices ----> to obtain a valid range of indices in a list or array.
+        println("${fruits[index].FruitName} is on index $index")
+    }
 }
